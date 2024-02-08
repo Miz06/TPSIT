987c334e282eec2d68f00306ffaaa554954ede62
Mizzon <alessandro.mizzon@iisviolamarchesini.edu.it>
2024-02-08 17:00:41 +0100

    .

--- ScriviFilePipe.c
+++ ScriviFilePipe.c
@@ -3,11 +3,13 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
+#define DIM_BUFFER 1048
 int main(int argc, char *argv[])
 {
-    int p;
+    char buffer[DIM_BUFFER];
+    int p, status;
     int fd[2];
-    int status;
+    int n; 
 
     if(pipe(fd) < 0)
     {
@@ -19,44 +21,51 @@ int main(int argc, char *argv[])
 
     if(p > 0)
     {
-        char line[255];
         FILE *FileS;
         FileS = fopen("memoryProcess.txt", "r");
 
         if(FileS == NULL)
         {
             perror("Errore nell'apertura del file.");
+
+            close(fd[0]);
+            close(fd[1]);
+
             exit(2);
         }
 
         close(fd[0]);
 
-        while(fgets(line, sizeof(line), FileS) != NULL)
+        while((n = fread(buffer, 1, sizeof(buffer), FileS)) > 0)
         {
-            write(fd[1], line, sizeof(line));
+            write(fd[1], buffer, n);
         }
                             
         close(fd[1]);
         fclose(FileS);
+
         wait(&status);
     }
     else if(p == 0)
     {
-        char line[255];
         FILE *FileD;
-        FileD = fopen("FileDEstinazione.txt", "w");
-
+        FileD = fopen("FileDestinazione.txt", "w");
+        
         if(FileD == NULL)
         {
             perror("Errore nell'apertura del file.");
+            
+            close(fd[0]);
+            close(fd[1]);
+
             exit(-2);
         }
 
         close(fd[1]);
 
-        while(read(fd[0], line, sizeof(line)) > 0)
+        while((read(fd[0], buffer, sizeof(buffer))) > 0)
         {
-            fprintf(FileD, "%s", line);
+            fwrite(buffer, 1, sizeof(buffer), FileD);
         }
 
         close(fd[0]);
@@ -65,6 +74,10 @@ int main(int argc, char *argv[])
     else
     {
         perror("Errore nel corso della fork");
+
+        close(fd[0]);
+        close(fd[1]);
+
         exit(1);
     }
 
