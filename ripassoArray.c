0dada9d06e4d9d21472f708914e98417a007d443
Alessandro Mizzon <122207463+Miz06@users.noreply.github.com>
2024-09-21 12:15:16 +0200

    .

--- ripassoArray.c
+++ ripassoArray.c
@@ -3,37 +3,37 @@
 #include <string.h>
 #include <time.h>
 
-void popolaArray(int vett[])
+void popolaArray(int vett[], int len)
 {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < len; i++)
     {
         vett[i] = rand() % 30;
     }
 }
 
-void stampaArray(int vett[])
+void stampaArray(int vett[], int len)
 { //1
     printf("\n");
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < len; i++)
     {
         printf("%d) %d\n", i, vett[i]);
     }
 }
 
-void stampaArrayInverso(int vett[])
+void stampaArrayInverso(int vett[], int len)
 { //2
     printf("\n");
-    for (int i = 9; i > -1; i--)
+    for (int i = len-1; i > -1; i--)
     {
         printf("%d) %d\n", i, vett[i]);
     }
 }
 
-int somma(int vett[])
+int somma(int vett[], int len)
 {//3
     int somma = 0;
 
-    for(int i = 0; i<10; i++){
+    for(int i = 0; i<len; i++){
         somma += vett[i];
     }
 
@@ -45,9 +45,9 @@ double media(int somma)
     return     (double)somma/10;
 }
 
-void stampaPari(int vett[])
+void stampaPari(int vett[], int len)
 {//4
-    for(int i = 0; i<10; i++)
+    for(int i = 0; i<len; i++)
     {
         if(vett[i] % 2 == 0){
             printf("%d\n", vett[i]);
@@ -55,9 +55,9 @@ void stampaPari(int vett[])
     }
 }
 
-void stampaDispari(int vett[])
+void stampaDispari(int vett[],int len)
 {//5
-    for(int i = 0; i<10; i++)
+    for(int i = 0; i<len; i++)
     {
         if(vett[i] % 2 != 0){
             printf("%d\n", vett[i]);
@@ -65,27 +65,70 @@ void stampaDispari(int vett[])
     }
 }
 
-int cercaNum(int vett[], int num)
+int cercaNum(int vett[], int num, int len)
 {//6
     int found = -1; 
 
-    for(int i = 0; i<10; i++){
+    for(int i = 0; i<len; i++){
         if(num == vett[i]){
             found = i;
-            i = 10;
+            i = len;
         }
     }
 
     return found;
 }
 
+int rimuoviElemento(int vett[], int num, int len)
+{//7
+    int found;
+    
+    found = cercaNum(vett, num, len); 
+ 
+    if(found != -1)
+    {
+        for(int i =found; i<len-1; i++)
+        {
+            vett[i] = vett[i+1];
+        }
+        
+        vett[len-1] = -1;
+    }
+    
+    return found;
+}
+
+void invertiCoppie(int vett[], int len)
+{//8
+    int sup;
+    
+    if(len % 2 == 0)
+    {
+        for(int i = 0; i<len; i+=2)
+        {
+            sup = vett[i];
+            vett[i] = vett[i+1];
+            vett[i+1] = sup;
+        }
+    }
+    else
+    {
+        for(int i = 0; i<len-1; i+=2)
+        {
+            sup = vett[i];
+            vett[i] = vett[i+1];
+            vett[i+1] = sup;
+        }
+    }
+}
 int main()
 {
+    int len = 9; 
     int arr[10];
     srand(time(0));
     int choice;
 
-    popolaArray(arr);
+    popolaArray(arr, len);
 
     do
     {
@@ -96,7 +139,8 @@ int main()
         printf("[4] Visualizza numeri pari\n");
         printf("[5] Visualizza numeri dispari\n");
         printf("[6] Ricerca numero\n");
-        
+        printf("[7] Elimina elemento\n");
+        printf("[8] Inverti ordine a coppie\n");
         printf("Scelta: ");
         scanf("%d", &choice);
 
@@ -104,26 +148,26 @@ int main()
         {
             if (choice == 1)
             {
-                stampaArray(arr);
+                stampaArray(arr, len);
             }
             else if (choice == 2)
             {
-                stampaArrayInverso(arr);
+                stampaArrayInverso(arr, len);
             }
             else if(choice == 3)
             {
-                int Somma = somma(arr);
+                int Somma = somma(arr, len);
                 printf("\nSomma: %d\n", Somma);
                 printf("Media: %f\n", media(Somma));
             }
             else if(choice == 4)
             {
                 printf("\n");
-                stampaPari(arr);
+                stampaPari(arr, len);
             }
             else if(choice == 5){
                 printf("\n");
-                stampaDispari(arr);
+                stampaDispari(arr, len);
             }
             else if(choice == 6){
                 int numRicerca; 
@@ -131,7 +175,7 @@ int main()
                 printf("\nNumero che si desidera ricercare: ");
                 scanf("%d", &numRicerca);
 
-                int position = cercaNum(arr, numRicerca);
+                int position = cercaNum(arr, numRicerca, len);
 
                 if(position == -1)
                 {
@@ -140,6 +184,25 @@ int main()
                 else{
                     printf("Il numero si trova in posizione %d\n", position);
                 }
+            }
+            else if(choice == 7)
+            {
+                int num; 
+                
+                printf("\nNumero da rimuovere: ");
+                scanf("%d", &num);
+                
+                if (rimuoviElemento(arr, num, len) != -1)
+                {
+                    len--;
+                }
+                else{
+                    printf("Numero non presente nell'array\n");
+                }
+            }
+            else if(choice == 8)
+            {
+                invertiCoppie(arr, len);
             }
         }
     } while (choice != 0);
